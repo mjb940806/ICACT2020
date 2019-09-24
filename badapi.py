2d2e88acc65f7c8b5594581f3e41c64da4de9e0e
ming <mjb940806@naver.com>
2019-09-24 11:13:26 +0900

    Edit folder name to list

--- badapi.py
+++ badapi.py
@@ -5,19 +5,18 @@ import sys
 
 s = "getenv|printf|strcpy|vfprintf|vsprintf|wcscat|wctomb|bsearch|gmtime|qsort|strerror|vfscanf|vsscanf|wcscpy|wmemcpy|localtime|setbuf|strncat|vfwprintf|vswprintf|wcsncat|wmemmove|fprintf|mbsrrtowcs|snprintf|strncpy|vfwscanf|vswscanf|wcsncpy|wprintf|fscanf|mbstowcs|sprintf|strtok|vprintf|vwprintf|wcsrtombs|wscanf|fwprintf|memcpy|sscanf|swprintf|vscanf|vwscanf|wcstok|fwscanf|memmove|strcat|swscanf|vsnprintf|wcrtomb|wcstombs"
 p = re.compile(s)
-count = 0
-total = 0
-filelist = sorted(glob.glob('C/testcases/CWE401_Memory_Leak/s*/*[0-9].c'))
-
-for i in filelist:
-    with open(i, 'r') as f:
-        total = total + 1
-        for x,y in enumerate(f.readlines(),1):
-            m = p.search(y)
-            if m:
-                print(i)
-                os.system("rm "+i)
-                count = count + 1
-                break
-
-print(str(count)+"/"+str(total))
+folderlist = ['121','122','124','126','127','190','191','369','415','416','476','681']
+for folder in folderlist:
+	filelist = sorted(glob.glob('C/testcases/CWE'+folder+'_*/s*/*[0-9].c'))
+	count = 0
+	total = 0
+	for i in filelist:
+	    with open(i, 'r') as f:
+	        total = total + 1
+	        for x,y in enumerate(f.readlines(),1):
+	            m = p.search(y)
+	            if m:
+	                os.system("rm "+i)
+	                count = count + 1
+	                break
+	print("CWE"+folder+": removed "+str(count)+" of "+str(total)+" files")
