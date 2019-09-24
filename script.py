e3bcc08fb05d544d33cedf668f694b4c94b188e9
ming <mjb940806@naver.com>
2019-09-24 23:07:49 +0900

    merge script for multi files and uni file

--- script.py
+++ script.py
@@ -3,17 +3,20 @@ import sys #running commands
 import glob #handling files
 
 filelist = sorted(glob.glob('s*/*[0-9].c'))
+if len(filelist) == 0: # if there is no s* folder
+    filelist = sorted(glob.glob('*[0-9].c'))
 
+unwind = sys.argv[1]
 count = 0
-f = open ('output.txt')
-f2 = open ('result.txt','w')
+f = open ('output_'+unwind+'.txt','w+t')
+f2 = open ('result_'+unwind+'.txt','w')
 #recursion start
-for filename in filelist: 
+for filename in filelist:
 
-    func = filename.split("/")
-    funcname = func[1]
+    func = filename.find("CWE")
+    funcname = filename[func:]
     funcname = funcname.replace(".c", "_bad")
-    os.system("cbmc "+filename+" --bounds-check --pointer-check --unwind 200 --function "+funcname+">> output.txt")
+    os.system("cbmc "+filename+" --bounds-check --pointer-check --pointer-overflow-check --unwind "+unwind+" --function "+funcname+">> output_"+unwind+".txt")
     data = f.read()
     result = data.split('\n')[-2]
     f2.write(filename+": "+result+"\n")
